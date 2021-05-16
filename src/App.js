2333a1afd5a1446dcc8f18c57d31261a3b517927
justinsmid <justin250899@hotmail.com>
2021-05-16 15:52:51 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+const isElectron = require('is-electron');
 
 function App() {
+  // Close the browser tab that's opened on launch in the actual browser and not in electron.
+  if (!isElectron()) {
+    window.close();
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.testjs</code> and save to reload.
         </p>
         <a
           className="App-link"
