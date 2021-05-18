7d3a4805aa7e8313d5f7ebdc6d87ca63b15d49cf
Justin Smid <justin250899@hotmail.com>
2021-05-18 10:34:16 +0200

    Read LCU data on startup

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,21 +1,26 @@
 const electron = require('electron');
-// Module to control application life.
-const app = electron.app;
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
-
 const path = require('path');
 const url = require('url');
+const LCUConnector = require('lcu-connector');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+const {app, BrowserWindow} = electron;
+
+const btoa = string => Buffer.from(string).toString('base64');
+
 let mainWindow;
 
 function createWindow() {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+    console.log('Creating electron window...');
 
-    // and load the index.html of the app.
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true
+        }
+    });
+
     const startUrl = process.env.ELECTRON_START_URL || url.format({
         pathname: path.join(__dirname, '/../build/index.html'),
         protocol: 'file:',
@@ -23,39 +28,59 @@ function createWindow() {
     });
     mainWindow.loadURL(startUrl);
 
-    // Open the DevTools.
     mainWindow.webContents.openDevTools();
 
-    // Emitted when the window is closed.
     mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
         mainWindow = null
-    })
+    });
+
+    readLCUData()
+        .then(data => {
+            console.log('LCU data received: ', data);
+            global.LCU_data = data;
+            global.LCU_auth = `${btoa(`${global.LCU_data.username}:${global.LCU_data.password}`)}`;
+        })
+        .catch(err => {
+            console.error(err);
+            electron.dialog.showErrorBox(
+                'LCU data reading timed out',
+                'This is probably because no open League client could be found. Please make sure your League client is running and restart this app.'
+            );
+            mainWindow.close();
+        });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
         app.quit()
     }
 });
 
 app.on('activate', function () {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
         createWindow()
     }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+const readLCUData = () => {
+    return new Promise((resolve, reject) => {
+        debugger;
+        const lcuConnector = new LCUConnector();
+
+        lcuConnector.on('connect', data => {
+            resolve(data);
+        });
+
+        lcuConnector.on('disconnect', () => {
+            console.warn(`WARN: LCU connector disconnected.`);
+        });
+
+        lcuConnector.start();
+
+        setTimeout(() => {
+            reject(`[LCU_TIMEOUT]: Express server timed out while attempting to read LCU data. Probably because no running League Client was found.`);
+        }, 10000);
+    });
+};
